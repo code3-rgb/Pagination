d491d55e59acab235f3f851bc3a98fff81120b54
aaron <noobestman33@gmail.com>
2022-11-24 12:37:52 +0530

    reactjs added

---
+++ client/pagination/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [posts,setPosts] = useState([])
+  const [loading,setLoading] = useState(false)
+  const [error,setError] = useState(false)
+  
+
+  return (
+    <div className="App">
+      {/* Pagination Component */}
+      {/* Posts listing */}
+      {/* Pagination Component */}
+    </div>
+  )
+}
+
+export default App
